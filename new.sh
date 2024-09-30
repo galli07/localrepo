8bae816e413ca4e510fe161bc8f20a4cc2368035
galli07 <150658093+galli07@users.noreply.github.com>
2024-10-01 02:47:28 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,3 +1,3 @@
 #! usr/bin/bash
 
-sudo pacman -S --needed vim alacritty xorg xorg-xinit man nitrogen i3 rofi firefox htop ttf-roboto apple-fonts
+sudo pacman -S --needed vim alacritty xorg xorg-xinit man networkmanager nitrogen i3 rofi firefox htop ttf-roboto
